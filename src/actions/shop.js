2509efef4e5d7d8d0c5f16974842679795695b5a
Kelsey Durmeier <KelseyDurmeier09@gmail.com>
2021-12-07 14:50:22 -0600

    fetch products action

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -1,4 +1,4 @@
-import { SET_NAVBAR_LINKS } from './types';
+import { SET_NAVBAR_LINKS, SET_SHOP_PRODUCTS } from './types';
 
 export function fetchShopCategories() {
     return ({
@@ -31,6 +31,63 @@ export function fetchShopCategories() {
             {
                 _id: 6,
                 title: 'Ruby'
+            }
+        ]
+    })
+}
+
+export function fetchShopProducts() {
+    return ({
+        type: SET_SHOP_PRODUCTS,
+        payload: [
+            {
+                _id: 0,
+                title: 'JavaScript in the Browser',
+                description: 'This is just dummy text for a description',
+                price: 1.99,
+                belongsTo: [0, 1]
+            },
+            {
+                _id: 1,
+                title: 'Graph Database',
+                description: 'This is just dummy text for a description',
+                price: 1.99,
+                belongsTo: [0, 6]
+            },
+            {
+                _id: 2,
+                title: 'Full Stack Development',
+                description: 'This is just dummy text for a description',
+                price: 1.99,
+                belongsTo: [0, 1, 4]
+            },
+            {
+                _id: 3,
+                title: 'User Interface Design',
+                description: 'This is just dummy text for a description',
+                price: 1.99 ,
+                belongsTo: [0, 2]
+            },
+            {
+                _id: 4,
+                title: 'JavaScript Development',
+                description: 'This is just dummy text for a description',
+                price: 1.99,
+                belongsTo: [0, 1]
+            },
+            {
+                _id: 5,
+                title: 'User Experience Design',
+                description: 'This is just dummy text for a description',
+                price: 1.99,
+                belongsTo: [0, 2]
+            },
+            {
+                _id: 6,
+                title: 'Advanced OOP',
+                description: 'This is just dummy text for a description',
+                price: 1.99,
+                belongsTo: [0, 6]
             }
         ]
     })
