71758a446be2cc449bd96d35a966660630aa61a4
Kelsey Durmeier <KelseyDurmeier09@gmail.com>
2021-12-17 09:39:20 -0600

    built review form details

--- src/components/order/review-form.js
+++ src/components/order/review-form.js
@@ -19,6 +19,24 @@ class ReviewForm extends Component {
                 <div className='review-form__line'></div>
                 <Field className='review-form__proceed' name='proceed' type='submit' title='Proceed to Checkout' component={FormButton} onClick={() => history.push('/signin')} />
                 <Field className='review-form__back' name='back' type='button' title='Back' short={true} component={FormButton} onClick={() => history.push('/shop')} />
+                <div className='review-form__details review-details'>
+                    <div className='review-details__subtotal review-detail'>
+                        <div className='review-detail__title'>Subtotal</div>
+                        <div className='review-detail__price'>$7.96</div>
+                    </div>
+                    <div className='reveiw-details__subtotal review-detail'>
+                        <div className='review-detail__title'>Tax</div>
+                        <div className='review-detail__price'>$0.16</div>
+                    </div>
+                    <div className='reveiw-details__subtotal review-detail'>
+                        <div className='review-detail__title'>Subtotal</div>
+                        <div className='review-detail__price'>$7.96</div>
+                    </div>
+                    <div className='review-details__total review-detail review-detail-green'>
+                        <div className='review-detail__title review-detail-green__title'>Total</div>
+                        <div className='review-detail__price review-detail-green__price'>$8.12</div>
+                    </div>
+                </div>
             </form>
         )
     }
