7faa0b737c205f9c7407cabd9502afdc8f3081e9
Kelsey Durmeier <KelseyDurmeier09@gmail.com>
2021-12-20 11:50:53 -0600

    built payment form

--- src/components/information/payment-form.js
+++ src/components/information/payment-form.js
@@ -3,16 +3,59 @@ import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton } from '../form-fields';
 import history from '../../history';
+import OrderSummary from '../information/order-summary';
 
 class PaymentForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
         return (
             <form onSubmit={handleSubmit} className={`${className} payment-form`}>
-                <Field className='payment-form__email' name='email' type='email' title='Email' placeholder='Email' component={FormInput} />
-                <Field className='payment-form__password' name='password' type='password' title='Password' placeholder='Password' component={FormInput} />
+                <Field className='payment-form__name'
+                type='name'
+                title='Name on Credit Card'
+                placeholder='Name'
+                name='name'
+                component={FormInput}
+                />
+                <Field className='payment-form__card'
+                type='card'
+                title='Credit Card Number'
+                placeholder='____-____-____-____'
+                name='card'
+                component={FormInput}
+                />
+                <Field className='payment-form__expiration'
+                type='expiration'
+                title='Expiration Date'
+                placeholder='Expiration'
+                name='expiration'
+                component={FormInput}
+                />
+                <Field className='payment-form__ccv'
+                type='ccv'
+                title='CCV'
+                placeholder='CCV'
+                name='ccv'
+                component={FormInput}
+                />
                 <div className='payment-form__line'></div>
-                <Field className='payment-form__login' name='login' type='submit' title='Login' component={FormButton} onClick={() => history.push('/account')} />
+                <Field className='payment-form__pay-complete'
+                onClick={() => history.push('information/payment')}
+                type='submit'
+                title='Pay & Complete'
+                name='pay-complete'
+                component={FormButton}
+                />
+                <Field className='payment-form__back'
+                onClick={() => history.push('information/payment')}
+                type='button'
+                title='Back'
+                name='back'
+                short={true}
+                component={FormButton}
+                />
+                <OrderSummary className='payment-form__order-summary' />
+
             </form>
         )
     }
